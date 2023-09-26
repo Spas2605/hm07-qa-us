b12f228c25f1403847ea2f85c2249f3e8f216e1a
Spas Gyorev <spas.gyorev@gmail.com>
2023-09-26 15:02:16 -0400

    Spas

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -18,7 +18,7 @@ const requestBody = {
 test('Should return 200 status code', async () => {
 	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/9`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
@@ -36,7 +36,7 @@ test('Should return 200 status code', async () => {
 test('Response body should be true', async () => {
 	let actualResponsBody;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/9`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
