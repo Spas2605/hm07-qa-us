a6f3552c7694a76c6dc1a4255e8b1709fc6db7e4
Spas Gyorev <spas.gyorev@gmail.com>
2023-10-04 15:34:04 -0400

    Gyorevvv

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://b48434d7-fb6e-4bc3-9ea9-5de4a8d83388.serverhub.tripleten-services.com"
+    API_URL: "https://a87f222a-ceb2-4e59-aa00-303ffdfae9fb.serverhub.tripleten-services.com"
 }
